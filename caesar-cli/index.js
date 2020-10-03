f39679c9c6b8dad1fbb8cd0650726d6ce044ec5e
Дмитрий <tomountain@mail.ru>
2020-10-03 18:41:25 +0400

    fix: add action_ to cli tools

--- caesar-cli/index.js
+++ caesar-cli/index.js
@@ -1,32 +1,22 @@
-// const { program } = require('commander');
 const commander = require('commander')
 const program = new commander.Command();
 
-// program
-//   .option('-s, --shift', 'a shift')
-//   .option('-i, --input', 'an input file')
-//   .option('-o, --output', 'an output file')
-//   .option('-a, --action', 'an action encode/decode');
-
-// program.parse(process.argv);
-
-// if (program.shift) console.log(program.opts())
-// console.log('shift')
-// if (program.input) console.log('input')
-// if (program.output) console.log('output')
-// if (program.action) console.log('action')
-
-
 program
-  .option('-s, --shift<number>', 'a shift')
-  .option('-i, --input<string>', 'an input file')
-  .option('-o, --output<string>', 'an output file')
-  .option('-a, --action<string>', 'an action encode/decode');
+  .option('-a, --action_ <string>', 'an action encode/decode')
+  .option('-s, --shift <number>', 'a shift')
+  .option('-i, --input <string>', 'an input file')
+  .option('-o, --output <string>', 'an output file')
+  .parse(process.argv);
+  
+try {
+  if (program.shift) console.log(program.shift);
+  if (program.input) console.log(program.input);
+  if (program.output) console.log(program.output);
+  if (program.action) console.log(program.action_);
+} catch(error) {
+  console.log(error)
+}
 
-program.parse(process.argv);
 
-if (program.shift) console.log('shift');
-if (program.input) console.log('input');
-if (program.output) console.log('output');
-if (program.action) console.log('action');
+
 
